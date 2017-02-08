19befedccb7a649498e7a4233dfe08d1046fd527
Tri Pham <phamqtri1992@gmail.com>
2017-02-08 15:29:37 -0800

    ninja gold v1
    Add status of click later

--- ninja_gold/ViewController.swift
+++ ninja_gold/ViewController.swift
@@ -10,9 +10,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var score = 0
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    @IBAction func farmButton(_ sender: UIButton) {
+        let ran = Int(arc4random_uniform(10)+10)
+        score += ran
+        scoreLabel.text = "Score: \(score)"
+    }
+    @IBAction func caveButton(_ sender: UIButton) {
+        let ran = Int(arc4random_uniform(5)+5)
+        score += ran
+        scoreLabel.text = "Score: \(score)"
+    }
+    @IBAction func houseButton(_ sender: UIButton) {
+        let ran = Int(arc4random_uniform(3)+2)
+        score += ran
+        scoreLabel.text = "Score: \(score)"
+    }
+    @IBAction func casinoButton(_ sender: UIButton) {
+        let ran = Int(arc4random_uniform(50))
+        let winorlose = Int(arc4random_uniform(2))
+        if winorlose == 1 {
+            score += ran
+        }
+        else {
+            score -= ran
+        }
+        scoreLabel.text = "Score: \(score)"
+    }
+    @IBAction func resetButton(_ sender: UIButton) {
+        score = 0
+        scoreLabel.text = "Score: \(score)"
+    }
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        scoreLabel.text = "Score: \(score)"
     }
 
     override func didReceiveMemoryWarning() {
